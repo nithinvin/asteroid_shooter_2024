99bd4c85f84553bf4dc4e062490f4aee43dc31f8
Nithin V <nithinvblr@gmail.com>
2024-04-14 20:42:10 +0530

    blank_screen

--- ast_shooter.py
+++ ast_shooter.py
@@ -1,1 +1,14 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+pygame.display.set_caption("Asteroid shooter")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
