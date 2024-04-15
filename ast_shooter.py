0791a7fda25017e5f74460e8ee3db0b4857538c6
Nithin V <nithinvblr@gmail.com>
2024-04-15 18:39:17 +0530

    color_surface

--- ast_shooter.py
+++ ast_shooter.py
@@ -3,12 +3,16 @@ import pygame, sys
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
 pygame.display.set_caption("Asteroid shooter")
+
+test_surf = pygame.Surface((400,100))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    display_surface.fill((15,140,122))        
+    display_surface.blit(test_surf,(1100,600))
+
     pygame.display.update()
