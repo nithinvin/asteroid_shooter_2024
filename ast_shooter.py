77385b0ddcef2c49cd6a098ee4b141aedc2614ae
Nithin V <nithinvblr@gmail.com>
2024-04-18 21:17:35 +0530

    more_movement

--- ast_shooter.py
+++ ast_shooter.py
@@ -7,7 +7,7 @@ pygame.display.set_caption("Asteroid shooter")
 clock = pygame.time.Clock()
 
 ship_surf = pygame.image.load("./graphics/ship.png").convert_alpha()
-ship_y_pos = 500
+ship_rect = ship_surf.get_rect(center =(640,360))
 back_surf = pygame.image.load("./graphics/background.png").convert()
 
 font = pygame.font.Font("./graphics/subatomic.ttf",50)
@@ -21,8 +21,9 @@ while True:
     clock.tick(120)
     display_surface.fill((0,0,0))
     display_surface.blit(back_surf,(0,0))
-    ship_y_pos -= 4
-    display_surface.blit(ship_surf,(300,ship_y_pos))
-    display_surface.blit(text_surf,(540,240))
+    if ship_rect.top > 0:
+       ship_rect.y -= 4
+    display_surface.blit(ship_surf,ship_rect)
+    display_surface.blit(text_surf,(500,200))
 
     pygame.display.update()
