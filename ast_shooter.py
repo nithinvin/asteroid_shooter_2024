5c7db13a191b6c014a4877d806499efe9a363aa8
Nithin V <nithinvblr@gmail.com>
2024-05-15 19:26:58 +0530

    laser_prep

--- ast_shooter.py
+++ ast_shooter.py
@@ -8,7 +8,9 @@ clock = pygame.time.Clock()
 
 ship_surf = pygame.image.load("./graphics/ship.png").convert_alpha()
 ship_rect = ship_surf.get_rect(center =(WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
+
 back_surf = pygame.image.load("./graphics/background.png").convert()
+
 laser_surf = pygame.image.load("./graphics/laser.png").convert_alpha()
 laser_rect = laser_surf.get_rect(midbottom = ship_rect.midtop)
 
@@ -16,22 +18,29 @@ font = pygame.font.Font("./graphics/subatomic.ttf",50)
 text_surf = font.render("Space", True, (255,255,255))
 text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH/2,WINDOW_HEIGHT-80))
 
+test_rect = pygame.Rect(100,200,400,500)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    clock.tick(120)
+    dt = clock.tick(120)/ 1000
 
     ship_rect.center = pygame.mouse.get_pos()
 
-    laser_rect.y -= 10
+    print(laser_rect.y)
+    print(dt)
+    laser_rect.y -= round(200 * dt)
 
     display_surface.fill((0,0,0))
     display_surface.blit(back_surf,(0,0))
-    display_surface.blit(ship_surf,ship_rect)
+
     display_surface.blit(text_surf,text_rect)
+    pygame.draw.rect(display_surface,(255,255,255),text_rect.inflate(30,30), width = 8, border_radius = 5)
+    
     display_surface.blit(laser_surf,laser_rect)
+    display_surface.blit(ship_surf,ship_rect)
 
     pygame.display.update()
